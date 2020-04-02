560d093140301d8d608307f96f9787ce014ba278
stefano Sossi <tenosossi2012@gmail.com>
2020-04-01 21:28:55 -0400

    Resolving conflicts in master

--- src/2.js
+++ src/2.js
@@ -1,5 +1,9 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 /* My File Edition Two */
 =======
 /***** My new Master File Edition *******/
 >>>>>>> Editing Second file 2.js in master
+=======
+/* My File Edition Two */
+>>>>>>> Resolving conflicts in master
